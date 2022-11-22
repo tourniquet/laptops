5ce553d8cae0fedb93e62c470936a8c65f51d2d5
tourniquet <admyn3d@gmail.com>
2022-11-22 19:02:02 +0000

    view and basic template for index page

--- laptop/views.py
+++ laptop/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Laptop
+
+def index(request):
+  laptops = Laptop.objects.order_by('created')
+  context = {'laptops': laptops}
+  return render(request, 'laptop/index.html', context)
+
