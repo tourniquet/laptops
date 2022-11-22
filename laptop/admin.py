acc9c0ed70c2b9a3d6fa4fa7ac9a8f0652a01de8
tourniquet <admyn3d@gmail.com>
2022-11-22 17:55:57 +0000

    added models to Admin panel

--- laptop/admin.py
+++ laptop/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Brand, Model, Laptop
+
+admin.site.register(Brand)
+admin.site.register(Model)
+admin.site.register(Laptop)
