2cac7fb47cf023b883c7095fe748cb9d7885737e
tourniquet <admyn3d@gmail.com>
2022-11-22 17:55:25 +0000

    created models for Brand, Model and Laptop

--- laptop/models.py
+++ laptop/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+  name = models.CharField(max_length=20, unique=True)
+
+  class Meta:
+    verbose_name_plural = 'brands'
+
+  def __str__(self):
+    return f'{self.name}'
+
+
+class Model(models.Model):
+  brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+  name = models.CharField(max_length=20, unique=True)
+
+  class Meta:
+    verbose_name_plural = 'models'
+
+  def __str__(self):
+    return f'{self.name}'
+
+
+class Laptop(models.Model):
+  title = models.CharField(max_length=100)
+  model = models.ForeignKey(Model, on_delete=models.CASCADE)
+  description = models.TextField()
+  status = models.BooleanField(default=True) # is it Sold or Available for sale
+  price = models.IntegerField()
+  quantity = models.IntegerField(default=1)
+
+  class Meta:
+    verbose_name_plural = 'laptops'
+
+  def __str__(self):
+    return f'{self.title}'
