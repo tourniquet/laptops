f17103e839025b3f9228cd799adf47f09b6aa712
tourniquet <admyn3d@gmail.com>
2022-11-28 21:18:44 +0000

    settings to serve static files like styles or images

--- laptop/urls.py
+++ laptop/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -6,5 +8,5 @@ app_name = 'laptop'
 urlpatterns = [
   # Home page
   path('', views.index, name='index'),
-  path('item/<int:id>/', views.item, name='item'),
-]
+  path('item/<int:id>/', views.item, name='item'), # use slug urls, like /dell-inspiron-5110 or /hp-pavilion-dv6
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
