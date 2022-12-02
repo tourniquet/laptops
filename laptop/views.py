e3dcef1d31eaa21d6324b979823e6fdc52d391bf
tourniquet <admyn3d@gmail.com>
2022-12-02 23:34:15 +0000

    order laptops from recent to oldest

--- laptop/views.py
+++ laptop/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Laptop
 
 def index(request):
-  laptops = Laptop.objects.order_by('created')
+  laptops = Laptop.objects.order_by('-created')
   context = {'laptops': laptops}
   return render(request, 'laptop/index.html', context)
 
